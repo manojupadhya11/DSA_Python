9884d0ef9c38cee934a10faf65a21b1beb282f80
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2024-12-17 22:26:58 +0530

    Reverse a list without reverse() function

---
+++ GFG Codes/Lists/01_Write a program or function to reverse the given list without using reverse() builtin method.py
@@ -0,0 +1,10 @@
+#01_Write a Python Program to reverse a list without using reverse() function
+#1.	Reverse a List
+#	Write a function to reverse a given list without using the reverse () method.
+
+
+mylist = ["Manoj","Manu","Greeshma","Sarada","Sandesh"]
+
+#mylist.reverse()
+
+print(mylist[::-1])
