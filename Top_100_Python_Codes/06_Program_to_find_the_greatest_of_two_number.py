4602f19254ea020eb7fc2a8778d13fe9c71a1ed5
manojupadhya11 <manojupadhya11@gmail.com>
2026-07-17 17:00:17 +0530

    program to find the greatest of two number

---
+++ Top_100_Python_Codes/06_Program_to_find_the_greatest_of_two_number.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 16:54:10 2026
+
+@author: Manu
+"""
+
+#Program to calculate greatest of two numbers in the given user inputted value
+
+num1 = int(input("Enter number 1 "))
+num2 = int(input("Enter number 2 "))
+
+if num1 > num2:
+    print(num1,"is greater than ",num2)
+else:
+    print(num2,"is the greater than ",num1)
